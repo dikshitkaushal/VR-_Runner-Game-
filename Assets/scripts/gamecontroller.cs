11de5efac119c30749b5280f0439e7c11b245bc9
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-08-30 13:31:21 +0530

    first commit

---
+++ Assets/scripts/gamecontroller.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gamecontroller : MonoBehaviour
+{
+    public player_movement player;
+    public AudioClip[] clips;
+    private AudioSource source;
+    public Camera fpcam;
+    AudioSource crowdvoice;
+    public TextMesh infotext;
+    private float gametimer = 0f;
+    public GameObject rematch;
+    private void Awake()
+    {
+        player.enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        crowdvoice = GameObject.Find("Player").GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();
+        start_the_race();
+        rematch.SetActive(false);
+    }
+
+    public void start_the_race()
+    {
+        crowdvoice.volume = 0.28f;
+        playaudio(0);
+        StartCoroutine(set());
+    }
+    IEnumerator set()
+    {
+        yield return new WaitForSeconds(1f);
+        playaudio(1);
+        StartCoroutine(gunshot());
+    }
+    IEnumerator gunshot()
+    {
+        yield return new WaitForSeconds(1.8f);
+        playaudio(2);
+        crowdvoice.volume = 0.6f;
+        player.enabled = true;
+    }
+    public void playaudio(int clip)
+    {
+        source.clip = clips[clip];
+        source.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player.reachedfinishedline)
+        {
+            gametimer += Time.deltaTime;
+            infotext.text = "Avoid the Obstacles!\nMove the Head Down to jump! \nYour Time : " + Mathf.Floor(gametimer);
+        }
+        else
+        {
+            
+            RaycastHit hit;
+            if (Physics.Raycast(fpcam.transform.position, fpcam.transform.forward, out hit))
+            {
+                if (hit.transform.CompareTag("restart"))
+                {
+                    restartgame();
+                }
+            }
+            infotext.text = "You Win!\nYour Time : " + Mathf.Floor(gametimer);
+            rematch.SetActive(true);
+            StartCoroutine(stopgame());
+        }
+    }
+    IEnumerator stopgame()
+    {
+        yield return new WaitForSeconds(1);
+        playaudio(3);
+        player.enabled = false;
+    }
+
+    public void restartgame()
+    {
+        player.enabled = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/scripts/player_movement.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed = 0f;
+    public float acceleration = 0.3f;
+    public float max_speed = 7f;
+    public float jumpingforce = 350f;
+    public bool isjumping=false;
+    public bool reachedfinishedline = false;
+    private float timer = 0f;
+    player_oscillation oscillate;
+    public Camera fpcam;
+    public float jumping_angle = 20.0f;
+    public Canvas damage;
+    private void Awake()
+    {
+        damage.enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        oscillate = GetComponentInChildren<player_oscillation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += acceleration * Time.deltaTime;
+        if (speed > max_speed)
+        {
+            speed = max_speed;
+        }
+        timer -= Time.deltaTime;
+        transform.position += speed * Vector3.forward * Time.deltaTime;
+        if(fpcam.transform.localEulerAngles.x >jumping_angle && isjumping==false && fpcam.transform.localEulerAngles.x < 90.0f)
+        {
+            jump();
+        }
+        if(timer<0)
+        {
+            isjumping = false;
+            oscillate.enabled = true;
+        }
+    }
+
+    public void jump()
+    {
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingforce);
+        isjumping = true;
+        oscillate.enabled = false;
+        timer = 1.35f;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("obstacle"))
+        {
+            speed *= 0.3f;
+            StartCoroutine(showdamage());
+        }
+       
+        if(other.CompareTag("finishingline"))
+        {
+            reachedfinishedline = true;
+        }
+    }
+    IEnumerator showdamage()
+    {
+        damage.enabled = true;
+        yield return new WaitForSeconds(1f);
+        damage.enabled = false;
+    }
+}
